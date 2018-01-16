44ef4ac27c935a7959b14f20261433d53e0df7e1
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-16 08:14:50 -0700

    added stylesheet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>css-snap assignment</title>
+	</head>
+	<body>
+		<p>This is a snap assignment.</p>
+		<p>It is to teach us about .css</p>
+		<p>It will refresh our memory on .css language</p>
+	</body>
+</html>
